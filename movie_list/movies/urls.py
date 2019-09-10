194ade4bbe0e395d3a89a72454c0d5f1c2764956
Justin Schoen <j23schoen@gmail.com>
2019-09-10 13:12:35 -0400

    created basic login

--- movie_list/movies/urls.py
+++ movie_list/movies/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -8,5 +9,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('movie_list/<int:movie_list_id>/', views.view, name='view'),
     path('movie_list/new', views.new_movie_list, name='new'),
-    path('movie_list/create', views.create, name='create')
+    path('movie_list/create', views.create, name='create'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('sign_up/', views.sign_up, name='sign_up')
 ]
--- movie_list/movies/views.py
+++ movie_list/movies/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login as log_user_in
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 from .models import MovieList
 from .dao.movie_list import get_movie_lists, get_movie_list
 
@@ -19,3 +23,16 @@ def create(request):
     movie_list = MovieList(name=request.POST['name'])
     movie_list.save()
     return view(request, movie_list.id)
+
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = authenticate(username=form.cleaned_data.get('username'), password=form.cleaned_data.get('password1'))
+            log_user_in(request, user)
+            return redirect('/')
+
+    form = UserCreationForm()
+    return render(request, 'registration/sign_up.html', {'form': form})
